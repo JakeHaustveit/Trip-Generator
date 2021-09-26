fa37d41a70c939a59c3e4277c0a697d314b37255
unknown <jacob.haustveit@gmail.com>
2021-09-26 10:29:24 -0400

    Added function that randomly generates new variable.

--- day trip generator.js
+++ day trip generator.js
@@ -4,46 +4,127 @@ let activities= ["Fly fishing", "Mountian biking", "Overnight Camping", "Bar Hop
 let newWheels= ["Ford F-150", "Jeep Grand Charokee", "Volkswagon Bus", "Mazda Miata"]
 
 
-function tripPlanner(){
-    console.log(`Your itinerary has you eating at  ${randomPicker(restaurant)}\n You will be driving a ${randomPicker(newWheels)}\n to ${randomPicker(destination)}\n where you will be doing ${randomPicker(activities)}\n Have a wonderful trip!`)
-   
-
-}
-
-
-
-
-// function resterauntPicker(){
-//     let resterauntRandom= resteraunt[Math.floor(Math.random() * resteraunt.length)]
-//     console.log(resterauntRandom)
-//   let resterauntAnswer= confirm (`Your restaurant is ${resterauntRandom}\n Do you want to eat here?`)
-//    while(resterauntAnswer != true){
-//     resterauntRandom= resteraunt[Math.floor(Math.random() * resteraunt.length)]
-//     let resterauntAnswerTwo= confirm("does this work?")
-//     if(resterauntAnswerTwo != true){
-//         confirm("does this work?");
-//         return resterauntRandom
-//     }
-//     else{
-//         return;
-//     }  
-//    }  
-   
+//main function arrayPicker
+// function randomPicker(array){
+//     let arrayItemRandom= array[Math.floor(Math.random() * array.length)]
+//     return arrayItemRandom;
 // }
+
 
 function randomPicker(array){
     let arrayItemRandom= array[Math.floor(Math.random() * array.length)]
     return arrayItemRandom;
+}
 
+
+
+
+
+
+
+
+
+
+
+let food= randomPicker(restaurant)
+let place= randomPicker(destination)
+let fun= randomPicker(activities)
+let whip= randomPicker(newWheels)
+
+
+
+ 
+(confirmPicker(food, randomPicker(restaurant)))
+(confirmPicker(place, randomPicker(destination)))
+(confirmPicker(fun, randomPicker(activities)))
+(confirmPicker(whip, randomPicker(newWheels)))
+
+
+function confirmPicker(random, array){
+    let randomItem= confirm(`Your option is ${random}\n Are you happy with this?`)
+    if(randomItem == true){
+        return(random);
+    }
+    else{
+        let randomItemTwo= randomPicker(array)
+        randomItemTwo= confirm(`Your new option is ${array}`)           
+    
+    return(array)}
 
 }
 
-randomPicker(restaurant)
+
+
+
+// function confirmPicker(random, array){
+//     let randomItem= confirm(`Your option is ${random}\n Are you happy with this?`)
+//     if(randomItem == true){
+//         return(random);
+//     }
+//     else{
+//         let randomItemTwo= randomPicker(array)
+//         randomItemTwo= confirm(`Your new option is ${array}\n Do you want this?`)
+//     while(randomItemTwo == true){
+//        let randomItemTWo= randomPicker(array)
+             
+//     }
+//     return(array)}
+
+// }
+
+
+
+
+
+
+//Trip Prompt
+function tripPlanner(){
+     alert(`Your itinerary has you eating at  ${randomPicker(restaurant)}\n You will be driving a ${randomPicker(newWheels)}\n to ${randomPicker(destination)}\n where you will be doing ${randomPicker(activities)}\n Have a wonderful trip!`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let resterauntAnswer= confirm (`Your restaurant is ${randomPicker(restaurant)}\n Do you want to eat here?`) 
+
+// let restaurantAnswerTwo
+
+// {
+//     while( resterauntAnswer != true){
+//         let restaurantAnswerTwo= confirm(`Your new restaurant is ${randomPicker(restaurant)}\n Does this work?`)
+//         if(restaurantAnswerTwo == true){
+//             let resterauntAnswer=== true
+//         }
+//         else{
+//             restaurantAnswerTwo= confirm(`Your new restaurant is ${randomPicker(restaurant)}\n Does this work?`);
+//     }
+//  }
+//  return(food)
+
+// }
+
+
+
 // randomPicker(destination)
 // randomPicker(activities)
 // randomPicker(newWheels)
 
-console.log(randomPicker(restaurant))
 
 
 // let restaurantTwo= confirm (`Your restaurant is ${randomPicker(restaurant)}\n Do you want to eat here?`)
@@ -55,19 +136,26 @@ console.log(randomPicker(restaurant))
 //     }
 //     else{console.log(randomPicker(restaurant))
 //     }
+// }
+       
+
+    
 
 
+// let destinationTwo= confirm(`Your destination is ${randomPicker(destination)}\n Do you want to go here?`)
+// while(destinationTwo !=true){
+//     randomPicker(destination)
+//     confirm(`Your new restaurant is ${randomPicker(destination)}`)
+// }
 
+// let activitiesTwo= confirm(`Your activity is ${randomPicker(destination)}\n Do you want to go here?`)
+// while(activitiesTwo !=true){
+//     randomPicker(destination)
+//     confirm(`Your new activity is ${randomPicker(activities)}`)
+// }
 
-let resterauntAnswer= confirm (`Your restaurant is ${randomPicker(restaurant)}\n Do you want to eat here?`)
-   while(resterauntAnswer != true){
-    resterauntRandom= restaurant[Math.floor(Math.random() * restaurant.length)]
-    let resterauntAnswerTwo= confirm("does this work?")
-    if(resterauntAnswerTwo != true){
-        confirm("does this work?");
-        return resterauntRandom
-    }
-    else{
-        return;
-    }
-}
+// let newWheelsTwo= confirm(`Your vehicle is ${randomPicker(newWheels)}\n Do you want to go here?`)
+// while(activitiesTwo !=true){
+//     randomPicker(newWheels)
+//     confirm(`Your new activity is ${randomPicker(newWheels)}`)
+// }
